74356687709f1d21b54e3f72805aadeb44ef8f5f
andrehebra <andrehebra@gmail.com>
2024-06-01 10:38:31 -0400

    changed clock in times

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -397,7 +397,7 @@
                     {#if user.Name != null}
                         <h1>{user.Name}</h1>
                         {#each user.times as clockInItem, index}
-                            {#if clockInItem.clockIn.toDate().getHours() > 6 || (clockInItem.clockIn.toDate().getHours() == 6 && clockInItem.clockIn.toDate().getMinutes() > 30)}
+                        {#if (clockInItem.clockIn.toDate().getHours() === 5) ||  (clockInItem.clockIn.toDate().getHours() === 13)}
                                 <div class="todo late">
                                     <div class="horiz-list">
                                         <DateInput  class={user.email + " " + index + " clockout"} value={clockInItem.clockIn.toDate()} /> - 
