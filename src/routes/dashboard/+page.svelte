3bfcb269c2e6f71eab9214f85074e409a6e7571e
andrehebra <andrehebra@gmail.com>
2024-06-04 09:41:08 -0400

    added datetime picker

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -381,6 +381,7 @@
                                         <DateInput  class={user.email + " " + index + " clockout"} value={clockInItem.clockIn.toDate()} /> - 
                                         {#if clockInItem.clockOut != "CURRENTLY CLOCKED IN"}
                                             <input type="date" />
+                                            <input type="datetime" />
                                         {:else}
                                             <p>Currently Clocked In</p>
                                         {/if}
